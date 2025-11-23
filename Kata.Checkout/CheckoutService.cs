0dd97a9eb859e0554beabcc697c1d5e7afeee27c
tomporter1 <tomporter62@gmail.com>
2025-11-23 16:07:24 +0000

    Added initial code outline for test writing

---
+++ Kata.Checkout/CheckoutService.cs
@@ -0,0 +1,24 @@
+﻿namespace Kata.Checkout;
+
+public class CheckoutService: ICheckoutService
+{
+    public CheckoutService()
+    {
+        
+    }
+
+    public void ScanItem(string item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveItem(string item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public float Total()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Kata.Checkout/ICheckoutService.cs
@@ -0,0 +1,8 @@
+﻿namespace Kata.Checkout;
+
+public interface ICheckoutService
+{
+    void ScanItem(string item);
+    void RemoveItem(string item);
+    float Total();
+}
